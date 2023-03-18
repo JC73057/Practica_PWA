69d019359fad578e61e976759c591d58d2734015
Jose Luis Castillo <joseluiscastillo1@gmail.com>
2023-03-18 13:05:17 -0500

    Final_M18

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ const showCoffees = () => {
   coffees.forEach(
     ({ name, image }) =>
       (output += `
-              <div class="col">
+              <div class="col-12">
                 <img class="card--avatar" src=${image} />
                 <h1 class="card--title">${name}</h1>
                 <a class="card--link" href="#">Comprar</a>
